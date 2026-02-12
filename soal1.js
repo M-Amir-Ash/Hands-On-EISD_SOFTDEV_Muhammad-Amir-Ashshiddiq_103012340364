e0204a03f89fce84064678f60fc17ca9ce8a4c60
M-Amir-Ash <145683856+M-Amir-Ash@users.noreply.github.com>
2026-02-12 21:50:53 +0700

    Fix comment syntax for soal1.js

---
+++ soal1.js
@@ -0,0 +1,22 @@
+// soal1.js
+const studyGroups = [
+  { nama: "Software Development", kuota: 20, terdaftar: 18 },
+  { nama: "UI/UX Design", kuota: 15, terdaftar: 15 },
+  { nama: "Technopreneur", kuota: 25, terdaftar: 10 },
+  { nama: "Intelligence System", kuota: 10, terdaftar: 10 },
+];
+
+function tampilkanKetersediaan(groups) {
+  const tersedia = [];
+  const penuh = [];
+
+  for (const g of groups) {
+    if (g.terdaftar < g.kuota) tersedia.push(g.nama);
+    else penuh.push(g.nama);
+  }
+
+  console.log("Masih tersedia:", tersedia);
+  console.log("Sudah penuh:", penuh);
+}
+
+tampilkanKetersediaan(studyGroups);
